f8cb71c337d4726555fb9e959e07a029ef920598
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-25 23:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor (species) {
+		this._species=species;
+	}
+	get species() {
+		return this._species;
+	}
+	makeSound() {
+        if(this.species=='cat') {this.purr()}
+		else {
+			this.bark();
+		}
+	}
+}
 
-class Dog extends Animal {}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+		constructor(species) {
+		super(species);
+	}
+
+	bark() {
+		console.log("woof");
+	}
+}
+
+class Cat extends Animal {
+	constructor(species) {
+		super(species);
+	}
+
+	purr() {
+		console.log("purr");
+	}
+}
+
 
 // Do not change the code below this line
 window.Animal = Animal;
